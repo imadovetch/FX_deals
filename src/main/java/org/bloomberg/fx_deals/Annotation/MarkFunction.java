2fccb593f01ed21bf42d79e59fb1f7d1a37ec9bd
imadovetch <ibouderoua63@gmail.com>
2025-08-03 21:38:14 +0100

    Adding Annotation MarkMethod

--- src/main/java/org/bloomberg/fx_deals/Annotation/MarkFunction.java
+++ src/main/java/org/bloomberg/fx_deals/Annotation/MarkFunction.java
@@ -1,4 +1,15 @@
 package org.bloomberg.fx_deals.Annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+
+// this annotation to mark methods using a unique name eg: for aop etc
+
 public @interface MarkFunction {
+    String value();
 }
